aa72719ad71ec8c03884e0dccf8910d3a2006d9a
Likai Liu <likai@likai.org>
2025-10-29 22:45:48 -0400

    Add strongs number links to biblehub.com

---
+++ Styles/Interlinear/Main.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  const xmlns = 'xmlns="http://www.w3.org/1999/xhtml"';
+  const words = document.getElementsByTagName('word');
+  for (let i = 0; i < words.length; ++i) {
+    const s = words[i].getElementsByTagName('strongs')[0];
+    const sn = s.textContent;
+    if (!sn) {
+      continue;
+    }
+
+    const isHebrew = words[i].getElementsByTagName('hebrew').length > 0;
+    const isGreek = words[i].getElementsByTagName('greek').length > 0;
+    let hrefPrefix;
+    if (isHebrew) {
+      hrefPrefix = 'https://biblehub.com/hebrew/';
+    } else if (isGreek) {
+      hrefPrefix = 'https://biblehub.com/greek/';
+    } else {
+      continue;
+    }
+    s.innerHTML = `<a ${xmlns} href="${hrefPrefix}/${sn}.htm">${sn}</a>`;
+  }
+}
