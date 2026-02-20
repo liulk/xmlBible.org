d6f44b6753e6fce9b9af363c819d0c032a795e51
Likai Liu <likai@likai.org>
2026-02-20 16:44:46 -0400

    add link from OT to Septuagint

--- Styles/Interlinear/Navigation.js
+++ Styles/Interlinear/Navigation.js
@@ -67,7 +67,76 @@ const BOOK_CHAPTERS = {
   '63-2 John': 1,
   '64-3 John': 1,
   '65-Jude': 1,
-  '66-Revelation': 22
+  '66-Revelation': 22,
+};
+
+const TLA_CODES = {
+  '01-Genesis': 'GEN',
+  '02-Exodus': 'EXO',
+  '03-Leviticus': 'LEV',
+  '04-Numbers': 'NUM',
+  '05-Deuteronomy': 'DEU',
+  '06-Joshua': 'JOS',
+  '07-Judges': 'JDG',
+  '08-Ruth': 'RUT',
+  '09-1 Samuel': '1SA',
+  '10-2 Samuel': '2SA',
+  '11-1 Kings': '1KI',
+  '12-2 Kings': '2KI',
+  '13-1 Chronicles': '1CH',
+  '14-2 Chronicles': '2CH',
+  '15-Ezra': 'EZR',
+  '16-Nehemiah': 'NEH',
+  '17-Esther': 'EST',
+  '18-Job': 'JOB',
+  '19-Psalms': 'PSA',
+  '20-Proverbs': 'PRO',
+  '21-Ecclesiastes': 'ECC',
+  '22-Song of Solomon': 'SNG',
+  '23-Isaiah': 'ISA',
+  '24-Jeremiah': 'JER',
+  '25-Lamentations': 'LAM',
+  '26-Ezekiel': 'EZK',
+  '27-Daniel': 'DAN',
+  '28-Hosea': 'HOS',
+  '29-Joel': 'JOL',
+  '30-Amos': 'AMO',
+  '31-Obadiah': 'OBA',
+  '32-Jonah': 'JON',
+  '33-Micah': 'MIC',
+  '34-Nahum': 'NAM',
+  '35-Habakkuk': 'HAB',
+  '36-Zephaniah': 'ZEP',
+  '37-Haggai': 'HAG',
+  '38-Zechariah': 'ZEC',
+  '39-Malachi': 'MAL',
+  '40-Matthew': 'MAT',
+  '41-Mark': 'MRK',
+  '42-Luke': 'LUK',
+  '43-John': 'JHN',
+  '44-Acts': 'ACT',
+  '45-Romans': 'ROM',
+  '46-1 Corinthians': '1CO',
+  '47-2 Corinthians': '2CO',
+  '48-Galatians': 'GAL',
+  '49-Ephesians': 'EPH',
+  '50-Philippians': 'PHP',
+  '51-Colossians': 'COL',
+  '52-1 Thessalonians': '1TH',
+  '53-2 Thessalonians': '2TH',
+  '54-1 Timothy': '1TI',
+  '55-2 Timothy': '2TI',
+  '56-Titus': 'TIT',
+  '57-Philemon': 'PHM',
+  '58-Hebrews': 'HEB',
+  '59-James': 'JAS',
+  '60-1 Peter': '1PE',
+  '61-2 Peter': '2PE',
+  '62-1 John': '1JN',
+  '63-2 John': '2JN',
+  '64-3 John': '3JN',
+  '65-Jude': 'JUD',
+  '66-Revelation': 'REV',
 };
 
 // The file paths are relative to the other Interlinear XML files.
@@ -121,6 +190,20 @@ const createNavigationLinkDenorm = () => {
 
   label.appendChild(check);
   return label;
+}
+
+// Link to LXX.
+
+const createNavigationLXX = () => {
+  const tla = TLA_CODES[CURR.book].toLowerCase();
+  const chapterNum = parseInt(CURR.chapter.substring(8, 11));
+  const a = document.createElementNS(XHTML_NS, "a");
+  a.id = 'linkLXX';
+  a.href = `https://www.blueletterbible.org/lxx/${tla}/${chapterNum}`;
+  a.title = 'Link to Septuagint text on Blue Letter Bible';
+  a.innerHTML = 'LXX';
+  a.setAttribute('testament', 'old');
+  return a;
 }
 
 const makeBookSelectOnChange = (hierarchy, chapterSelect, goSubmit) => {
@@ -243,6 +326,7 @@ const initializeNavigation = () => {
   }
   nav.appendChild(aNext);
 
+  nav.appendChild(createNavigationLXX());
   nav.appendChild(createNavigationGreekUppercase());
   nav.appendChild(createNavigationLinkDenorm());
 
